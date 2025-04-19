6507fe08e718e7792a25408e4e2c1774c7c5a5da
Pablo Mederos <pablomedeross@gmail.com>
2025-04-18 22:38:39 -0300

    Add NuGet Release workflow and update Mediator interface to support cancellation

--- src/RouteDispatcher/ConcreteServices/Mediator.cs
+++ src/RouteDispatcher/ConcreteServices/Mediator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using RouteDispatcher.Contracts;
+using System.Threading;
 
 namespace RouteDispatcher.ConcreteServices
 {
@@ -13,8 +14,11 @@ namespace RouteDispatcher.ConcreteServices
             _serviceProvider = serviceProvider;
         }
 
-        public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request)
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
         {
+            if(cancellationToken.IsCancellationRequested)
+                return Task.FromCanceled<TResponse>(cancellationToken);
+
             var requestType = request.GetType();
             var handlerType = typeof(IRequestHandler<,>)
                 .MakeGenericType(requestType, typeof(TResponse));
@@ -24,10 +28,11 @@ namespace RouteDispatcher.ConcreteServices
                 ?? throw new InvalidOperationException($"No handler found for request type {requestType.Name}");
 
             var methodInfo = handlerType.GetMethod("Handle");
-            
-            var result = await (Task<TResponse>)methodInfo.Invoke(handler, new[] { request });
 
-            return result;
+            if(cancellationToken.IsCancellationRequested)
+                return Task.FromCanceled<TResponse>(cancellationToken);
+            
+            return (Task<TResponse>)methodInfo.Invoke(handler, new object[] { request, cancellationToken });
         }
     }
 }
--- src/RouteDispatcher/Contracts/IMediator.cs
+++ src/RouteDispatcher/Contracts/IMediator.cs
@@ -1,9 +1,10 @@
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RouteDispatcher.Contracts
 {
     public interface IMediator
     {
-        Task<TResponse> Send<TResponse>(IRequest<TResponse> request);
+        Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
     }
 }
